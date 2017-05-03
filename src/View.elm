ae49cea3ab9711e9e42f1b771e20bfadb211b637
jjelfs <jjelfs@travelrepublic.co.uk>
2017-05-03 20:07:11 +0100

    adding svg

--- src/View.elm
+++ src/View.elm
@@ -4,10 +4,20 @@ import Actions exposing (..)
 import Html exposing (Html, text, div, img)
 import Html.Attributes exposing (src)
 import ViewModel exposing (Model)
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
 
 view : Model -> Html Msg
 view model =
     div []
         [ img [ src model.logo ] []
-        , div [] [ text model.message ]
+        , div
+            []
+            [roundRect]
         ]
+
+roundRect : Html Msg
+roundRect =
+    svg
+      [ width "120", height "120", viewBox "0 0 120 120" ]
+      [ rect [ x "10", y "10", width "100", height "100", rx "15", ry "15" ] [] ]
