17bf0df7846a7034e963ef5ec252f2f6d7f3d15c
jjelfs <jjelfs@travelrepublic.co.uk>
2017-05-04 21:29:22 +0100

    added a note

--- src/View.elm
+++ src/View.elm
@@ -7,28 +7,38 @@ import ViewModel exposing (Model)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 
+stave =
+    [10,13,16,19,22,30,33,36,39,42]
+        |> List.map toString
+        |> List.map
+            (\n ->
+                Svg.path
+                    [ strokeWidth "0.2"
+                    , stroke "black"
+                    , d ("M 10 " ++ n ++ " L 90 " ++ n)]
+                    []
+            )
+
+currentNote model =
+    [ circle
+        [ cx "50"
+        , cy "11.5"
+        , r "1"
+        , stroke "black"
+        , fill "black" ]
+        []
+    ]
+
 view : Model -> Html Msg
 view model =
     let
         (w, h) =
             (model.windowSize.width, model.windowSize.height)
 
-        stave =
-            [10,13,16,19,22,30,33,36,39,42]
-                |> List.map toString
-                |> List.map
-                    (\n ->
-                        Svg.path
-                            [ strokeWidth "0.2"
-                            , stroke "black"
-                            , d ("M 10 " ++ n ++ " L 90 " ++ n)]
-                            []
-                    )
     in
-        --svg element width and height defines the viewport
         svg
             [ width "100%"
             , height "100%"
             , viewBox "0 0 100 100"
             ]
-            stave
+            (stave ++ (currentNote model))
